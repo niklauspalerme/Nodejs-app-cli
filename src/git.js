1e46c22b60769b9441cffcd6d2e1fc40d42a290d
Palermo Rodriguez <nicolas.palermo@kcc.com>
2023-03-06 17:55:51 -0300

    Push #2

--- src/git.js
+++ src/git.js
@@ -1,1 +1,26 @@
-import {exec} from 'node'
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util'
+
+
+//Convertimos esta funcion async con el promisify
+const execAsync = promisify(exec);
+
+function cleanStdout(stdout){
+
+    return stdout.trim().split('\n').filter(Boolean)
+} 
+
+
+export async function getChangesFiles () {
+
+    const {stdout} = await execAsync('git status --porcelain')
+    return cleanStdout(stdout).map((line)=> line.split(' ').at(-1))
+}
+
+
+
+//Notas:
+
+//stdout (Standard Ouput) es el nombre que se coloca a una salida estandar de
+//datos luego de su ejecución
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import {intro,text,select, outro } from "@clack/prompts";
 import { COMMIT_TYPES } from "./commints-type.js";
 import colors from "picocolors"
+import { getChangesFiles } from "./git.js";
+import { trytm } from "@bdsqqq/try";
 
+
+const [changedFiles, errorChangedFiles] = await trytm (getChangesFiles())
+
+if(errorChangedFiles){
+  outro(colors.red(`Error: Comprueba que estás en un repositorio de git`));
+  process.exit(1);
+}
+
+console.log(changedFiles);
 
 intro(
 
@@ -11,7 +22,7 @@ intro(
 
 
 const commintType = await select({
-    message: 'Selecciona el tipo de commint',
+    message: colors.cyan ('Selecciona el tipo de commint:'),
     options: Object.entries(COMMIT_TYPES).map(([key,value]) => ({
         value: key,
         label: `${value.emoji} ${key} - ${value.description}`
